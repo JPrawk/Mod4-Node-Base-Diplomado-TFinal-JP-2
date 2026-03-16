c8fb7ec63bbfc23b0632a1aa684baff5056abe1a
Juan P Guzman <jgguzman1979@gmail.com>
2026-03-16 15:03:24 -0400

    fix: corregir errores en loggerjs

--- src/logs/logger.js
+++ src/logs/logger.js
@@ -1,12 +1,14 @@
 import pino from "pino";
 
-const logger = pino ({
-    transport: {
+const logger = pino({
+  transport: {
         target: "pino-pretty",
         options: {
-            translateTime: "SYS:dd/mm/yyyy HH:mm:ss"
-        }
-    }
+          translateTime: "SYS:dd/mm/yyyy HH:mm:ss",
+        },
+      },
+      
+   
 });
 
 export default logger;
