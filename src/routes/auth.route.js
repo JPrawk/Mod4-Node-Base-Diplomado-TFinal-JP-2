91951ba30ab066e3f824c531200f07960654057f
Juan P Guzman <jgguzman1979@gmail.com>
2026-03-16 03:14:02 -0400

    hasta clase 5

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import authController from "../controllers/auth.controller.js";
+import validate from "../validators/validate.js";
+import { createUserSchema } from "../validators/user.validate.js"; 
 
 const router = Router ();
 
-router.post("/", authController.login);
+router.post("/", validate(createUserSchema),authController.login);
 
 export default router
